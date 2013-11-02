c9f9e6bc0e527ce8a68aee06ecf7e6001092e9d6
Michał Kiełbowicz <jupblb@gmail.com>
2013-11-02 21:46:55 +0100

    Initial export

---
+++ src/HandRankBot.java
@@ -0,0 +1,189 @@
+import java.util.ArrayList;
+
+
+public final class HandRankBot extends HandRank
+{
+	public HandRankBot(ArrayList<Card> cards){
+		super(cards);
+	}
+	
+	public HandRankBot(Hand x){
+		super(x);
+	}
+	
+	public ArrayList<Card> getChangeList(ArrayList<Card> cards)
+	{
+		for(int i = 0 ; i < 9 ; i ++)
+		{
+			if(missingCards[i] == 1 || missingCards[i] == 0)
+			{
+				switch(i){
+				case 0: return getStraightFlush(cards);
+				case 1: return getFour(cards);
+				case 2: return getFullHouse(cards);
+				case 3: return getFlush(cards);
+				case 4: return getStraight(cards);
+				case 5: return getThree(cards);
+				case 6: return getTwoPair(cards);
+				case 7: return getOnePair(cards);
+				case 8: return getHighCard(cards);
+				}
+			}
+		}
+		
+		return null;
+	}
+	
+	public ArrayList<Card> getChangeList(Hand x){
+		return getChangeList(x.getHand());
+	}
+	
+	private ArrayList<Card> getHighCard(ArrayList<Card> cards)
+	{
+		ArrayList<Card> ret = new ArrayList<Card>();
+		
+		for(int i = 0 ; i < cards.size() ; i ++)
+		{
+			if(cards.get(i).getCardValue() != bestCard[8].getCardValue()){
+				ret.add(cards.get(i));
+			}
+		}
+		
+		return ret;
+	}
+
+	private ArrayList<Card> getOnePair(ArrayList<Card> cards)
+	{
+		ArrayList<Card> ret = new ArrayList<Card>();
+		
+		for(int i = 0 ; i < cards.size() ; i ++)
+		{
+			if(cards.get(i).getCardValue() != bestCard[7].getCardValue()){
+				ret.add(cards.get(i));
+			}
+		}
+		
+		return ret;
+	}
+
+	private ArrayList<Card> getTwoPair(ArrayList<Card> cards)
+	{
+		ArrayList<Card> ret = new ArrayList<Card>();
+		
+		for(int i = 0 ; i < cards.size() ; i ++)
+		{
+			if(cards.get(i).getCardValue() != bestCard[6].getCardValue() && cards.get(i).getCardValue() != secondBestCard[0].getCardValue()){
+				ret.add(cards.get(i));
+			}
+		}
+		
+		return ret;
+	}
+
+	private ArrayList<Card> getThree(ArrayList<Card> cards)
+	{
+		ArrayList<Card> ret = new ArrayList<Card>();
+		
+		for(int i = 0 ; i < cards.size() ; i ++)
+		{
+			if(cards.get(i).getCardValue() != bestCard[5].getCardValue()){
+				ret.add(cards.get(i));
+			}
+		}
+		
+		return ret;
+	}
+
+	private ArrayList<Card> getStraight(ArrayList<Card> cards)
+	{
+		ArrayList<Card> ret = new ArrayList<Card>();
+		
+		int minValue = 13;
+		for(int i = 0 ; i < cards.size() ; i ++)
+		{
+			if(cards.get(i).getCardValue() > bestCard[4].getCardValue()-5 && cards.get(i).getCardValue() <= bestCard[4].getCardValue()){
+				minValue = Math.min(minValue, cards.get(i).getCardValue());
+			}
+			if(bestCard[4].getCardValue() < 4 && cards.get(i).getCardValue() == 12){
+				minValue = -1;
+			}
+		}
+		
+		for(int i = 0 ; i < cards.size(); i ++)
+		{
+			if(!(cards.get(i).getCardValue() >= minValue && cards.get(i).getCardValue() < minValue+5)){
+				ret.add(cards.get(i));
+			}
+		}
+		
+		return ret;
+	}
+
+	private ArrayList<Card> getFlush(ArrayList<Card> cards)
+	{
+		ArrayList<Card> ret = new ArrayList<Card>();
+		
+		for(int i = 0 ; i < cards.size() ; i ++)
+		{
+			if(cards.get(i).getColorValue() != bestCard[3].getColorValue()){
+				ret.add(cards.get(i));
+			}
+		}
+		
+		return ret;
+	}
+
+	private ArrayList<Card> getFullHouse(ArrayList<Card> cards)
+	{
+		ArrayList<Card> ret = new ArrayList<Card>();
+		
+		for(int i = 0 ; i < cards.size() ; i ++)
+		{
+			if(cards.get(i).getCardValue() != bestCard[2].getCardValue() && cards.get(i).getCardValue() != secondBestCard[1].getCardValue()){
+				ret.add(cards.get(i));
+			}
+		}
+		
+		return ret;
+	}
+
+	private ArrayList<Card> getFour(ArrayList<Card> cards)
+	{
+		ArrayList<Card> ret = new ArrayList<Card>();
+		
+		for(int i = 0 ; i < cards.size() ; i ++)
+		{
+			if(cards.get(i).getCardValue() != bestCard[1].getCardValue()){
+				ret.add(cards.get(i));
+			}
+		}
+		
+		return ret;
+	}
+
+	private ArrayList<Card> getStraightFlush(ArrayList<Card> cards)
+	{
+		ArrayList<Card> ret = new ArrayList<Card>();
+		
+		int minValue = 13;
+		for(int i = 0 ; i < cards.size() ; i ++)
+		{
+			if(cards.get(i).getCardValue() > bestCard[0].getCardValue()-5 && cards.get(i).getCardValue() <= bestCard[0].getCardValue() && cards.get(i).getColorValue() == bestCard[0].getColorValue()){
+				minValue = Math.min(minValue, cards.get(i).getCardValue());
+			}
+			if(bestCard[0].getCardValue() < 4 && cards.get(i).getCardValue() == 12 && bestCard[0].getColorValue() == cards.get(i).getColorValue()){
+				minValue = -1;
+			}
+		}
+		
+		for(int i = 0 ; i < cards.size(); i ++)
+		{
+			if(!(cards.get(i).getCardValue() >= minValue && cards.get(i).getCardValue() < minValue+5 && cards.get(i).getColorValue() == bestCard[0].getColorValue())){
+				ret.add(cards.get(i));
+			}
+		}
+		
+		return ret;
+	}
+	
+}
---
+++ src/Main.java
@@ -0,0 +1,104 @@
+import java.util.Scanner;
+
+/**
+ * Main class of Pokerable game.
+ * @author Piotr Staniów
+ * @author Micha³ Kie³bowicz
+ */
+final class Main
+{
+	private static Scanner input;
+	/**
+	 * Default constructor.
+	 */
+	private Main() {
+	}
+	/**
+	 * @param args define whether program should be client or server
+	 * @throws Exception Lack of arguments
+	 */
+	public static void main(String[] args) throws Exception {
+		System.out.println("Welcome to Pokerable the game.");
+		input = new Scanner(System.in);
+		
+		if (args.length == 0)
+			throw new Exception("You didn't specify whether want to run either as server or client.");
+		if (args[0].equals("client")) {
+			int port=1700;
+			String host="localhost";
+			if (args.length==3) {
+				try {
+					port = Integer.parseInt(args[2]);
+				}
+				catch (NumberFormatException ex) {
+					System.err.println("Incorrect port number");
+					ex.printStackTrace();
+				}
+				host = args[1];
+			}
+			
+			System.out.println("Do you wish to be bot or human?");
+			String s = input.next();
+			
+			if (s.equals("human")) {
+				System.out.println("Trying to act like a human...");
+				try {
+					Player c = new Human(host, port);
+					c.start();
+				}
+				catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+			else if (s.equals("bot")) {
+				System.out.println("Bot mechanics are getting involved in...");
+				try {
+					Player c = new Bot(host, port);
+					c.start();
+				}
+				catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+			else {
+				System.out.println("Incorrect input - you'd rather write \"bot\" or \"human\".");
+				throw new Exception("Incorrect type of player selected");
+			}
+		}
+		else if (args[0].equals("server")) {
+			System.out.println("Trying to run server...");
+			//TODO jUnit test
+			Server s = null;
+			int port = 1700;
+			if (args.length==2) {
+				try {
+					port = Integer.parseInt(args[1]);
+				}
+				catch (NumberFormatException ex) {
+					System.err.println("Incorrect port number");
+					ex.printStackTrace();
+				}
+			}
+			try {
+				s = new Server(port);
+			}
+			catch (Exception e) {
+				System.err.println("Unable to start server on specified port. Trying to start on 1700");
+				try {
+					s = new Server();
+				}
+				catch (Exception ex) {
+					System.err.println("Unable to start server on port 1700. Closing...");
+					e.printStackTrace();
+					System.exit(-1);
+				}
+			}
+			s.start();
+		}
+		else {
+			System.out.println("Incorrect parameter. Game will be shut down. " + args[0]);
+			return;
+		}
+	}
+
+}
